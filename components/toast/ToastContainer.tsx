07c2e2095d03b6d32efbd791dcd7b9611a45f234
obedbolak <jamesnormankate@gmail.com>
2026-06-19 22:23:23 +0100

    kates

--- components/toast/ToastContainer.tsx
+++ components/toast/ToastContainer.tsx
@@ -3,10 +3,12 @@ import { useToast } from "@/hooks/useToast";
 import ToastItem from "./ToastItem";
 
 export default function ToastContainer() {
-  const { toasts } = useToast();
+  const { toasts, removeToast } = useToast();
   return (
     <div className="fixed bottom-4 right-4 z-50 flex flex-col gap-2">
-      {toasts.map((t) => <ToastItem key={t.id} toast={t} />)}
+      {toasts.map((t) => (
+        <ToastItem key={t.id} toast={t} onClose={() => removeToast(t.id)} />
+      ))}
     </div>
   );
 }
